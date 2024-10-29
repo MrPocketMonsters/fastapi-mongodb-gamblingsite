8af1744b59b788db3e9d9b0470f6aebf58cfd17d
Nicolás Sabogal <mew-four@hotmail.com>
2024-10-28 20:11:43 -0500

    Just some documentation.

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from fastapi import FastAPI
 from routes.route import router
 from config.database import client
 
+# Pings database to ensure the connection is up, then starts the application.
 try:
     client.admin.command('ping')
     app = FastAPI()
     app.include_router(router)
+
 except Exception as e:
     print(e)
--- schema/schemas.py
+++ schema/schemas.py
@@ -1,9 +1,11 @@
 def individual_serial(document:dict, model) -> dict:
+    '''Returns a dictionary with the "id" key and all fields in the provided model.'''
     result = {"id": str(document["_id"])}
     for key in model.__fields__.keys():
         result[key] = document[key]
 
     return result
 
-def list_serial(documents, model) -> list:
+def list_serial(documents, model) -> list[dict]:
+    '''Returns a list with the individual_serial(document, model) for each document in Cursor documents.'''
     return [individual_serial(document, model) for document in documents]
